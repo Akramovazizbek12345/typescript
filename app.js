6763be0ba34920e192a17015186112b7d9d6c7f0
AzizbekAkramov <akramovazizbek12345@gmail.com>
2025-05-21 20:42:38 +0300

    Sammi 2-video

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 "use strict";
-let surname = 'Azizbek';
-let classic = true;
-let age = 15;
-console.log(surname);
-console.log(classic);
-console.log(age);
+// video-1
+// let surname :string = 'Azizbek'
+// let classic :boolean = true
+// let age :Number = 15
+// console.log(surname);
+// console.log(classic);
+// console.log(age);
+// video-2
+const narx = 15000;
+const chegirma = 5000;
+const ayrilganchegirma = narx - chegirma;
+console.log(ayrilganchegirma);
--- app.ts
+++ app.ts
@@ -1,10 +1,17 @@
-let surname :string = 'Azizbek'
-let classic :boolean = true
-let age :Number = 15
+// video-1
 
-console.log(surname);
-console.log(classic);
-console.log(age);
+// let surname :string = 'Azizbek'
+// let classic :boolean = true
+// let age :Number = 15
 
+// console.log(surname);
+// console.log(classic);
+// console.log(age);
 
+// video-2
 
+const narx: number = 15000
+const chegirma: number = 5000
+
+const ayrilganchegirma: Number = narx - chegirma
+console.log(ayrilganchegirma)
