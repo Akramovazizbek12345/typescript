2d41d8ba4b86d5e8cbd8e5160694432e90118d17
AzizbekAkramov <akramovazizbek12345@gmail.com>
2025-05-21 20:26:11 +0300

    sammi 1-video

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict";
+let surname = 'Azizbek';
+let classic = true;
+let age = 15;
+console.log(surname);
+console.log(classic);
+console.log(age);
---
+++ app.ts
@@ -0,0 +1,10 @@
+let surname :string = 'Azizbek'
+let classic :boolean = true
+let age :Number = 15
+
+console.log(surname);
+console.log(classic);
+console.log(age);
+
+
+
